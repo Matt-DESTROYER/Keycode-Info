3706df893bd59fea9015ccce12614e0ca747629c
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2021-09-22 18:51:17 +1000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-document.onkeydown = function(e) {
+document.addEventListener("keydown", function (e) {
 	e = e || window.event;
 	document.getElementById("key").textContent = "Key/Action: " + e.key;
 	document.getElementById("keycode").textContent = "Key code: " + e.keyCode;
 	document.getElementById("keylocation").textContent = "Key location: " + e.location;
-}
+});
