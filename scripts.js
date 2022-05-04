10bd2f3f62c2d97ef8cc50d079065214f1dd3e4d
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2022-05-04 17:48:40 +1000

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("keydown", function (e) {
-	e = e || window.event;
-	document.getElementById("key").textContent = "Key/Action: " + e.key;
-	document.getElementById("keycode").textContent = "Key code: " + e.keyCode;
-	document.getElementById("keylocation").textContent = "Key location: " + e.location;
-});
